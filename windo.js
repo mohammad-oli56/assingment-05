da5b892aeaae6fe5ba67ac81baf0aaf33a98092a
Mohammad oli56 <olimohammad286@gmai.com>
2025-03-02 02:28:17 +0600

    final add

---
+++ windo.js
@@ -0,0 +1,8 @@
+document.getElementById('windo')
+    .addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    
+    
